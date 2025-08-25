d25acd865a610c486af5803287ff49662b88f84d
HYUK741 <dnwlslzx12@gmail.com>
2025-08-25 10:52:32 +0900

    date update

---
+++ app/dtos/create_participant_response.py
@@ -0,0 +1,18 @@
+import datetime
+import uuid
+
+from pydantic import BaseModel
+
+from app.dtos.frozen_config import FROZEN_CONFIG
+
+
+class ParticipantMysql(BaseModel):
+    model_config = FROZEN_CONFIG
+    id: int
+    date: datetime.date
+
+
+class CreateParticipantMysqlResponse(BaseModel):
+    model_config = FROZEN_CONFIG
+    participant_id: int
+    participant_dates: list[ParticipantMysql]
